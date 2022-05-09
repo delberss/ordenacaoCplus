b1d7e18aa759d0d14447449dda89463b089b9395
Delber Soares <delberss@hotmail.com>
2022-05-09 11:28:13 -0300

    mergeSort

--- main.cpp
+++ main.cpp
@@ -2,6 +2,46 @@
 
 using namespace std;
 
+void merge(int *vetor, int baixo, int meio, int alto)
+{
+    int temporario[alto + 1];
+    int i = baixo;
+    int j = meio + 1;
+    int k = 0;
+
+    while (i <= meio && j <= alto)
+    {
+        if (vetor[i] <= vetor[j])
+            temporario[k++] = vetor[i++];
+        else
+            temporario[k++] = vetor[j++];
+    }
+    while (i <= meio)
+        temporario[k++] = vetor[i++];
+    while (j <= alto)
+        temporario[k++] = vetor[j++];
+    k--;
+    while (k >= 0)
+    {
+        vetor[k + baixo] = temporario[k];
+        k--;
+    }
+}
+
+void mergeSort(int *vetor, int baixo, int alto)
+{
+    int meio;
+
+    if (baixo < alto)
+    {
+        meio = (baixo + alto) / 2;
+        mergeSort(vetor, baixo, meio);
+        mergeSort(vetor, meio + 1, alto);
+        merge(vetor, baixo, meio, alto);
+    }
+}
+
+
 void selecao(int vet[],int n){
     // PEGA O PRIMEIRO ELEMENTO, ASSUME ELE COMO MENOR
     // VAI VASCULHANDO O RESTO DO VETOR PARA ACHAR QUAL O MENOR
@@ -65,12 +105,14 @@ string imprimeVetor(int *vetor, int tamanhoVetor){
 
 int main(){
 
-    int n=13;
-    int vet[] = {3,6,5,1,2,8,7,9,4,10,13,12,11};
+    int n=14;
+    int vet[] = {44, 3,6,5,1,2,8,7,9,4,10,13,12,11};
 
     //selecao(vet,n);
-    insertionSort(vet,n);
+    //insertionSort(vet,n);
     //bubbleSort(vet,n);
+
+    mergeSort(vet, 0, n - 1);
 
     imprimeVetor(vet, n);
 
